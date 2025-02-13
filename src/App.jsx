1c81c67ebb7d3705728de7cf9d3e7fcd318c4997
조성범 <joseongbeom@joseongbeom-ui-MacBookPro.local>
2025-02-13 16:27:42 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
     if (wholeImages.length > 0) {
       setImagesToAnnotate(getRandomImages(wholeImages));
     }
-  }, [wholeImages]);
+  }, []);
   
 
   // 구글 Apps Script 웹 앱 URL (배포 후 발급받은 URL)
