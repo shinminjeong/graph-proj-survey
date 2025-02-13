4e9d58756c746709cdd76f35beb1c5f530253feb
조성범 <joseongbeom@joseongbeom-ui-MacBookPro.local>
2025-02-13 15:12:01 +0900

    수정좀..

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/graph-proj-survey',
+  base: '/graph-proj-survey/',
 })
