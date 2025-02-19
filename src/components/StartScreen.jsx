ae1e761eb46f777763f2a656bcb487b12128a1cc
조성범 <joseongbeom@joseongbeom-ui-MacBookPro.local>
2025-02-19 19:42:18 +0900

    fix: 문구 수정

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 function StartScreen({ onStart }) {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Welcome ...</h1>
+      <h1>Drawing Bounding Box in Graph</h1>
+      <p style={{ margin: '20px 0', fontSize: '16px', color: '#555' }}>
+        - Draw a Bounding Box around the area catches your attention by mouse dragging<br />
+        - You should draw a Box to move for the next graph image<br />
+        - If you drew the box incorrectly, you can redraw it to overwrite the previous one
+      </p>
+      
       <button onClick={onStart}>Start</button>
     </div>
   );
