cac88089ee152fe4c1eddbcb99b519eafe3a03b3
조성범 <joseongbeom@joseongbeom-ui-MacBookPro.local>
2025-03-22 17:50:55 +0900

    fix: 배포시 이미지 안보이는 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,8 @@ function App() {
     fetchSixImages();
   }, []);
 
-  const IMAGE_BASE_URL = '/graph-proj-survey/src/assets/images/';
+  // const IMAGE_BASE_URL = '/graph-proj-survey/src/assets/images/';
+  const IMAGE_BASE_URL = import.meta.env.BASE_URL + 'src/assets/images/';
   const mainStudyImages = useMemo(() => {
     if (sixImages && Array.isArray(sixImages) && sixImages.length > 0) {
       // sixImages를 이용해 파일 경로 리스트 생성
