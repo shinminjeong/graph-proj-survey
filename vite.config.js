035ba49deb04158981eebcb681854629fdbee154
Minjeong Shin <berebere86@gmail.com>
2025-08-06 13:33:45 +0900

    test -- survey app update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/survey/personal_meme_survey/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
   plugins: [react()],
-  base: '/personal_meme_survey/',
 })
